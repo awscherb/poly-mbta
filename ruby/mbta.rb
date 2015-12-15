18198a10c6492ba4b9ff5bc75205c1b4112da17b
awscherb <awscherb@gmail.com>
2015-12-15 13:47:43 -0500

    spacing

--- ruby/mbta.rb
+++ ruby/mbta.rb
@@ -5,8 +5,8 @@ require 'date'
 station_code = ARGV[0]
 
 if station_code.nil? or station_code.empty?
-	puts("You must enter a station code!")
-	exit
+  puts("You must enter a station code!")
+  exit
 end
 
 url = "http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=5VJxw0cEAEWShNPVo-iAtg&stop=" + station_code + "&format=json"
@@ -18,14 +18,14 @@ stop_name = result["stop_name"]
 puts("Arrival information for: " + stop_name)
 
 result["mode"].each do |mode|
-	puts("+- " + mode["mode_name"])
-	mode["route"].each do |route|
-		puts("  +- " + route["route_name"]) 
-		route["direction"].each do |direction|
-			puts("    +- " + direction["direction_name"])
-			direction["trip"].each do |trip|
-				puts("      +- " + Time.at(trip["sch_arr_dt"].to_i).strftime("%I:%M:%S"))
-			end
-		end
-	end
+  puts("+- " + mode["mode_name"])
+  mode["route"].each do |route|
+    puts("  +- " + route["route_name"]) 
+    route["direction"].each do |direction|
+      puts("    +- " + direction["direction_name"])
+      direction["trip"].each do |trip|
+        puts("      +- " + Time.at(trip["sch_arr_dt"].to_i).strftime("%I:%M:%S"))
+      end
+    end
+  end
 end 
